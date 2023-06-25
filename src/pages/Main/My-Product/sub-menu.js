d541bbc5d11626cf8d5e9f131c03de86636e0865
Piyakarn Nimmakulvirut (Jimmy) <jirakarnjim1@gmail.com>
2023-06-25 22:55:47 +0700

    feat: add button setting tab

--- src/pages/Main/My-Product/sub-menu.js
+++ src/pages/Main/My-Product/sub-menu.js
@@ -8,19 +8,31 @@ import {
   CardBody,
   CardTitle,
   Label,
+  Button,
 } from "reactstrap";
 import ColorPicker from "@vtaits/react-color-picker";
 import ClickAwayListener from "react-click-away-listener";
 
 const SubMenu = () => {
-  const [backgroundColorEnable, setBackgroundColorEnable] = useState(false);
-  const [backgroundColor, setBackgroundColor] = useState("#3b82f6");
-  const onDragBackgroundColor = color => {
-    setBackgroundColor(color);
+  const [backgroundColorEnable, setBackgroundColorEnable] = useState([]);
+  const [backgroundColor, setBackgroundColor] = useState([]);
+  const onDragBackgroundColor = (color, index) => {
+    let newBackgroundColor = [...backgroundColor];
+    newBackgroundColor[index] = color;
+    setBackgroundColor(newBackgroundColor);
   };
-  const closeBackgroundColorPicker = () => {
-    setBackgroundColorEnable(false);
+  const closeBackgroundColorPicker = index => {
+    let newBackgroundColorEnable = [...backgroundColorEnable];
+    newBackgroundColorEnable[index] = false;
+    setBackgroundColorEnable(newBackgroundColorEnable);
   };
+
+  const openBackgroundColorPicker = index => {
+    let newBackgroundColorEnable = [...backgroundColorEnable];
+    newBackgroundColorEnable[index] = true;
+    setBackgroundColorEnable(newBackgroundColorEnable);
+  };
+
   const subMenues = [
     { id: 1, title: "Facebook" },
     { id: 2, title: "Line" },
@@ -28,16 +40,59 @@ const SubMenu = () => {
     { id: 4, title: "Youtube" },
   ];
 
-  const [selectedMenues, setSelectedMenues] = useState([1, 2]);
+  const [selectedMenues, setSelectedMenues] = useState([]);
 
-  const selectMenu = e => {
+  const selectMenu = (e, index) => {
     const thisId = parseInt(e.target.value);
     if (selectedMenues.includes(thisId)) {
       const newSelectedMenues = [...selectedMenues].filter(i => i !== thisId);
       setSelectedMenues(newSelectedMenues);
+      setBackgroundColor(
+        backgroundColor.filter((i, idx) => {
+          return idx !== index;
+        })
+      );
     } else {
+      setBackgroundColor([...backgroundColor, "#3b82f6"]);
       setSelectedMenues([...selectedMenues, thisId]);
     }
+  };
+
+  // Custom
+  const [customMenues, setCustomMenues] = useState([]);
+  const [customBackgroundColorEnable, setCustomBackgroundColorEnable] =
+    useState([]);
+  const [customBackgroundColor, setCustomBackgroundColor] = useState([]);
+  const onDragCustomBackgroundColor = (color, index) => {
+    let newCustomBackgroundColor = [...customBackgroundColor];
+    newCustomBackgroundColor[index] = color;
+    setCustomBackgroundColor(newCustomBackgroundColor);
+  };
+  const closeCustomBackgroundColorPicker = index => {
+    let newCustomBackgroundColorEnable = [...customBackgroundColorEnable];
+    newCustomBackgroundColorEnable[index] = false;
+    setCustomBackgroundColorEnable(newCustomBackgroundColorEnable);
+  };
+
+  const openCustomBackgroundColorPicker = index => {
+    let newCustomBackgroundColorEnable = [...customBackgroundColorEnable];
+    newCustomBackgroundColorEnable[index] = true;
+    setCustomBackgroundColorEnable(newCustomBackgroundColorEnable);
+  };
+
+  const addCustomMenu = () => {
+    setCustomMenues([...customMenues, { title: "Custom" }]);
+    setCustomBackgroundColor([...customBackgroundColor, "#3b82f6"]);
+    setCustomBackgroundColorEnable([...customBackgroundColorEnable, false]);
+  };
+
+  const removeCustomMenu = index => {
+    let newCustomMenues = [...customMenues];
+    newCustomMenues.splice(index, 1);
+    setCustomMenues(newCustomMenues);
+    let newCustomBackgroundColor = [...customBackgroundColor];
+    newCustomBackgroundColor.splice(index, 1);
+    setCustomBackgroundColor(newCustomBackgroundColor);
   };
 
   return (
@@ -57,7 +112,7 @@ const SubMenu = () => {
                           type="checkbox"
                           id={subMenu.title}
                           value={subMenu.id}
-                          onChange={selectMenu}
+                          onChange={e => selectMenu(e, index)}
                           checked={selectedMenues.includes(subMenu.id)}
                         />
                         <label
@@ -74,7 +129,7 @@ const SubMenu = () => {
             </CardBody>
           </Card>
         </Col>
-        {selectedMenues.map(selectedMenu => {
+        {selectedMenues.map((selectedMenu, index) => {
           return (
             <Col md={12}>
               <Card>
@@ -89,23 +144,23 @@ const SubMenu = () => {
                         <Input
                           type="text"
                           className="colorpicker-default"
-                          value={backgroundColor}
+                          value={backgroundColor[index]}
                           readOnly
                         />
                         <div
-                          onClick={() => {
-                            setBackgroundColorEnable(!backgroundColorEnable);
-                          }}
+                          onClick={() => openBackgroundColorPicker(index)}
                           className="btn"
                           style={{
-                            backgroundColor: backgroundColor,
+                            backgroundColor: backgroundColor[index],
                             width: 40,
                             height: 40,
                           }}
                         ></div>
-                        {backgroundColorEnable ? (
+                        {backgroundColorEnable[index] ? (
                           <ClickAwayListener
-                            onClickAway={closeBackgroundColorPicker}
+                            onClickAway={() =>
+                              closeBackgroundColorPicker(index)
+                            }
                           >
                             <>
                               <ColorPicker
@@ -117,8 +172,10 @@ const SubMenu = () => {
                                 }}
                                 saturationHeight={100}
                                 saturationWidth={100}
-                                value={backgroundColor}
-                                onDrag={onDragBackgroundColor}
+                                value={backgroundColor[index]}
+                                onDrag={color =>
+                                  onDragBackgroundColor(color, index)
+                                }
                               />
                             </>
                           </ClickAwayListener>
@@ -159,6 +216,115 @@ const SubMenu = () => {
             </Col>
           );
         })}
+      </Row>
+      <Row>
+        {customMenues.map((customMenu, index) => {
+          return (
+            <Col md={12}>
+              <Card>
+                <CardBody>
+                  <CardTitle>
+                    <div className="d-flex justify-content-between">
+                      <span>{customMenu.title}</span>
+                      <Button
+                        onClick={() => removeCustomMenu(index)}
+                        type="button"
+                        className="btn btn-danger btn-sm"
+                      >
+                        Remove
+                      </Button>
+                    </div>
+                  </CardTitle>
+                  <Row>
+                    <Col md={2}>
+                      <Label>Background color</Label>
+                      <div className="d-flex gap-2">
+                        <Input
+                          type="text"
+                          className="colorpicker-default"
+                          value={customBackgroundColor[index]}
+                          readOnly
+                        />
+                        <div
+                          onClick={() => openCustomBackgroundColorPicker(index)}
+                          className="btn"
+                          style={{
+                            backgroundColor: customBackgroundColor[index],
+                            width: 40,
+                            height: 40,
+                          }}
+                        ></div>
+                        {customBackgroundColorEnable[index] ? (
+                          <ClickAwayListener
+                            onClickAway={() =>
+                              closeCustomBackgroundColorPicker(index)
+                            }
+                          >
+                            <>
+                              <ColorPicker
+                                style={{
+                                  position: "absolute",
+                                  right: 10,
+                                  marginTop: "2.8rem",
+                                  zIndex: 500,
+                                }}
+                                saturationHeight={100}
+                                saturationWidth={100}
+                                value={customBackgroundColor[index]}
+                                onDrag={color =>
+                                  onDragCustomBackgroundColor(color, index)
+                                }
+                              />
+                            </>
+                          </ClickAwayListener>
+                        ) : null}
+                      </div>
+                    </Col>
+                    <Col md={2}>
+                      <Label>Icon</Label>
+                      <select className="form-control">
+                        <option>Select Icon</option>
+                        <option>User</option>
+                      </select>
+                    </Col>
+                    <Col md={2}>
+                      <Label>Text</Label>
+                      <Input
+                        className="form-control"
+                        placeholder="Text"
+                      ></Input>
+                    </Col>
+                    <Col md={3}>
+                      <Label>Description</Label>
+                      <Input
+                        className="form-control"
+                        placeholder="Description"
+                      ></Input>
+                    </Col>
+                    <Col md={3}>
+                      <Label>Destination</Label>
+                      <Input
+                        className="form-control"
+                        placeholder="Destination"
+                      ></Input>
+                    </Col>
+                  </Row>
+                </CardBody>
+              </Card>
+            </Col>
+          );
+        })}
+        <Col md={12}>
+          <div className="d-grid gap-2">
+            <Button
+              onClick={addCustomMenu}
+              type="button"
+              className="btn btn-success"
+            >
+              Add Custom
+            </Button>
+          </div>
+        </Col>
       </Row>
     </Container>
   );
