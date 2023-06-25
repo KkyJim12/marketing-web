85d49bc4d2ba023c58305bab6ab67a8e39913d7b
Piyakarn Nimmakulvirut (Jimmy) <jirakarnjim1@gmail.com>
2023-06-25 23:22:11 +0700

    feat: add license tab

--- src/pages/Main/My-Product/license.js
+++ src/pages/Main/My-Product/license.js
@@ -1,5 +1,33 @@
+import { Card, CardBody, CardTitle, Row, Col, Button } from "reactstrap";
+
 const License = () => {
-  return <></>;
+  return (
+    <>
+      <Card>
+        <CardBody>
+          <CardTitle>
+            <div className="d-flex justify-content-between">
+              <span>Attached Script</span>
+              <Button type="button" className="btn btn-success">
+                Re new
+              </Button>
+            </div>
+          </CardTitle>
+          <Row className="gap-2 mt-2">
+            <Col md={12}>
+              <p>Domain: 3</p>
+              <p>Type: Floating Action Button</p>
+              <p>Duration: 30 days</p>
+              <p>Start Date: 25/06/2023</p>
+              <p>Expire Date: 25/07/2023</p>
+              <p>Expired In: 30 days</p>
+              <p>Status: On Going</p>
+            </Col>
+          </Row>
+        </CardBody>
+      </Card>
+    </>
+  );
 };
 
 export default License;
