da2f0d1bce8ce00c73ed2fbe0ef77d8a0f3c9aa2
Piyakarn Nimmakulvirut (Jimmy) <jirakarnjim1@gmail.com>
2023-07-12 06:15:39 +0700

    feat: show customize page content

--- src/pages/Main/Info/index.js
+++ src/pages/Main/Info/index.js
@@ -1,18 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Row, Col, Container } from "reactstrap";
 import Breadcrumbs from "../../../components/Common/Breadcrumb";
 import { useTranslation } from "react-i18next";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import Parser from "html-react-parser";
 
 const Info = () => {
   document.title = " Info | Marketing tool platform";
   const { t } = useTranslation();
+  const { id } = useParams();
+
+  const [page, setPage] = useState("");
+
+  useEffect(() => {
+    const getPage = async () => {
+      try {
+        const response = await axios.get(
+          `${process.env.REACT_APP_API_URL}/api/v1/user/pages/${id}`
+        );
+
+        setPage(response.data.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    getPage();
+  }, [id]);
+
   return (
     <React.Fragment>
       <div className="page-content">
         <Container fluid>
-          <Breadcrumbs title={t("Platform Name")} breadcrumbItem={t("Info")} />
+          <Breadcrumbs title={t("Platform Name")} breadcrumbItem={page.name} />
           <Row>
-            <Col md={12}></Col>
+            <Col md={12}>{page.content && Parser(page.content)}</Col>
           </Row>
         </Container>
       </div>
