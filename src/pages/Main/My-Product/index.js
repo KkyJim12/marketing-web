f6e68d36c6e305121f1cee53280783eed5293de9
Piyakarn Nimmakulvirut (Jimmy) <jirakarnjim1@gmail.com>
2023-07-13 05:29:07 +0700

    feat: add my product api

--- src/pages/Main/My-Product/index.js
+++ src/pages/Main/My-Product/index.js
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import moment from "moment";
+import axios from "axios";
 import { MDBDataTable } from "mdbreact";
 import { Row, Col, Card, CardBody, CardSubtitle, Container } from "reactstrap";
 import { Link } from "react-router-dom";
@@ -8,144 +10,153 @@ import { useTranslation } from "react-i18next";
 //Import Breadcrumb
 import Breadcrumbs from "../../../components/Common/Breadcrumb";
 
-const ReNewButton = () => {
-  const { t } = useTranslation();
-  return (
-    <button
-      className="btn btn-success waves-effect waves-light btn-sm"
-      type="button"
-    >
-      {t("Re new")}
-    </button>
-  );
-};
-
-const ManageButton = () => {
-  const { t } = useTranslation();
-  return (
-    <Link to="/my-product/1/manage">
-      <button
-        className="btn btn-info waves-effect waves-light btn-sm"
-        type="button"
-      >
-        {t("Manage")}
-      </button>
-    </Link>
-  );
-};
-
-const data = {
-  columns: [
-    {
-      label: "Package Name",
-      field: "packageName",
-      sort: "asc",
-      width: 150,
-    },
-    {
-      label: "Type",
-      field: "type",
-      sort: "asc",
-      width: 270,
-    },
-    {
-      label: "Domains",
-      field: "domains",
-      sort: "asc",
-      width: 270,
-    },
-    {
-      label: "Duration",
-      field: "duration",
-      sort: "asc",
-      width: 270,
-    },
-    {
-      label: "Start Date",
-      field: "startDate",
-      sort: "asc",
-      width: 270,
-    },
-    {
-      label: "Expire Date",
-      field: "expireDate",
-      sort: "asc",
-      width: 270,
-    },
-    {
-      label: "Expire In",
-      field: "expireIn",
-      sort: "asc",
-      width: 270,
-    },
-    {
-      label: "Status",
-      field: "status",
-      sort: "asc",
-      width: 270,
-    },
-    {
-      label: "Re New",
-      field: "reNew",
-      sort: "asc",
-      width: 270,
-    },
-    {
-      label: "Manage",
-      field: "manage",
-      sort: "asc",
-      width: 270,
-    },
-  ],
-  rows: [
-    {
-      id: 1,
-      packageName: "Floating action button #1",
-      type: "Floating action button",
-      domains: 3,
-      duration: 30,
-      startDate: "15/06/2023",
-      expireDate: "15/07/2023",
-      expireIn: "30 days",
-      status: "On going",
-      reNew: "",
-      manage: <ManageButton />,
-    },
-    {
-      id: 2,
-      packageName: "Floating action button #1",
-      type: "Floating action button",
-      domains: 3,
-      duration: 30,
-      startDate: "15/06/2023",
-      expireDate: "15/07/2023",
-      expireIn: "",
-      status: "Expired",
-      reNew: <ReNewButton />,
-      manage: <ManageButton />,
-    },
-  ],
-};
-
-const DataTable = () => {
-  return (
-    <React.Fragment>
-      <Card>
-        <CardBody>
-          <CardSubtitle className="mb-3">
-            List of purchased products.
-          </CardSubtitle>
-
-          <MDBDataTable responsive bordered data={data} noBottomColumns />
-        </CardBody>
-      </Card>
-    </React.Fragment>
-  );
-};
-
 const MyProduct = () => {
   const { t } = useTranslation();
   document.title = " My Product | Marketing tool platform";
+
+  const ReNewButton = (props) => {
+    if (props.status === "On going") {
+      return <>-</>;
+    } else {
+      return (
+        <button
+          className="btn btn-success waves-effect waves-light btn-sm"
+          type="button"
+        >
+          {t("Re new")}
+        </button>
+      );
+    }
+  };
+
+  const ManageButton = (props) => {
+    if (props.status === "On going") {
+      return (
+        <Link to="/my-product/1/manage">
+          <button
+            className="btn btn-info waves-effect waves-light btn-sm"
+            type="button"
+          >
+            {t("Manage")}
+          </button>
+        </Link>
+      );
+    } else {
+      return <>-</>;
+    }
+  };
+
+  const initData = {
+    columns: [
+      {
+        label: "Package Name",
+        field: "name",
+        sort: "asc",
+        width: 150,
+      },
+      {
+        label: "Type",
+        field: "type",
+        sort: "asc",
+        width: 270,
+      },
+      {
+        label: "Domains",
+        field: "domains",
+        sort: "asc",
+        width: 270,
+      },
+      {
+        label: "Duration",
+        field: "duration",
+        sort: "asc",
+        width: 270,
+      },
+      {
+        label: "Start Date",
+        field: "startDate",
+        sort: "asc",
+        width: 270,
+      },
+      {
+        label: "Expire Date",
+        field: "expireDate",
+        sort: "asc",
+        width: 270,
+      },
+      {
+        label: "Expire In",
+        field: "expireIn",
+        sort: "asc",
+        width: 270,
+      },
+      {
+        label: "Status",
+        field: "status",
+        sort: "asc",
+        width: 270,
+      },
+      {
+        label: "Re New",
+        field: "reNew",
+        sort: "asc",
+        width: 270,
+      },
+      {
+        label: "Manage",
+        field: "manage",
+        sort: "asc",
+        width: 270,
+      },
+    ],
+    rows: [],
+  };
+
+  const [data, setData] = useState(initData);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const getMyProducts = async () => {
+    try {
+      const headers = {
+        Authorization: localStorage.getItem("accessToken"),
+      };
+      const response = await axios.get(
+        `${process.env.REACT_APP_API_URL}/api/v1/user/my-products`,
+        { headers }
+      );
+
+      const fetchData = response.data.data;
+      const clonedData = initData;
+
+      for (let i = 0; i < fetchData.length; i++) {
+        const newData = {
+          name: fetchData[i].name,
+          type: fetchData[i].type,
+          domains: fetchData[i].domains,
+          duration: fetchData[i].duration,
+          startDate: moment(fetchData[i].startDate).format(
+            "DD/MM/YYYY, h:mm a"
+          ),
+          expireDate: moment(fetchData[i].endDate).format("DD/MM/YYYY, h:mm a"),
+          expireIn: moment(fetchData[i].endDate).fromNow(),
+          status: fetchData[i].status,
+          reNew: <ReNewButton status={fetchData[i].status} />,
+          manage: <ManageButton status={fetchData[i].status} />,
+        };
+
+        clonedData.rows.push(newData);
+      }
+
+      setData(clonedData);
+      setIsLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getMyProducts();
+  }, [isLoading]);
 
   return (
     <React.Fragment>
@@ -157,7 +168,21 @@ const MyProduct = () => {
           />
           <Row>
             <Col md={12}>
-              <DataTable />
+              <Card>
+                <CardBody>
+                  <CardSubtitle className="mb-3">
+                    List of purchased products.
+                  </CardSubtitle>
+                  {isLoading === false && (
+                    <MDBDataTable
+                      responsive
+                      bordered
+                      data={data}
+                      noBottomColumns
+                    />
+                  )}
+                </CardBody>
+              </Card>
             </Col>
           </Row>
         </Container>
