d058e560035aa66013abb2b73b29277d990b8e7b
Piyakarn Nimmakulvirut (Jimmy) <jirakarnjim1@gmail.com>
2024-01-07 11:00:02 +0700

    feat: add check exp auth

--- src/routes/middleware/Authmiddleware.js
+++ src/routes/middleware/Authmiddleware.js
@@ -1,11 +1,18 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 // import { Navigate } from "react-router-dom";
-const Authmiddleware = props => {
+const Authmiddleware = (props) => {
   const navigate = useNavigate();
+
+  const payload = localStorage.getItem("accessToken").split(".")[1];
+  const decoded = atob(payload);
+  const expTime = JSON.parse(decoded).exp;
+  const now = new Date();
+
   if (
     !localStorage.getItem("authUser") ||
-    !localStorage.getItem("accessToken")
+    !localStorage.getItem("accessToken") ||
+    now > expTime
   ) {
     navigate("/login");
   }
